f257730d7705f4905b897c74a0101575515c6d7b
jxorange <37055360+jxorange@users.noreply.github.com>
2021-03-29 14:47:11 +0800

    modify something

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN='fsw-final.us.auth0.com'
 export API_AUDIENCE='myapp'
 export ALGORITHMS=['RS256']
-export DATABASE_URL='postgresql://jxorange@localhost:5432/casting'
+export DATABASE_URL='postgres://jxorange@localhost:5432/casting'
